a8201543240d017962ed3afaa892df51576c0d91
jayotis <jayotis@outlook.com>
2025-06-12 21:38:27 -0600

    enable anonymous analytics use

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.firebase.crashlytics")
 }
 
 // We don't need the top-level keystore loading block,
